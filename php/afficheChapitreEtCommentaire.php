75f058ad3102ae6ddb894b113a7606275cec1ef7
Elise Rousseau <rousseaujumelle@gmail.com>
2019-10-24 16:45:49 +0200

    Rajout des commentaires pour chaque billet

--- php/afficheChapitreEtCommentaire.php
+++ php/afficheChapitreEtCommentaire.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Billet simple pour l'Alaska - Jean Forteroche</title>
+</head>
+<body>
+  
+<h2>Chapitre <?php echo $_GET["billet"] ?></h2>
 <?php
 
 $bdd = new PDO('mysql:host=localhost;dbname=blog; charset=utf8', 'root', '');
@@ -13,4 +25,26 @@ if(isset($_GET['billet'])){
 }
 
 
+
+echo '<h2>Commentaires</h2>';
+
+$requete =$bdd->prepare('SELECT * FROM commentaires WHERE id=? ');
+$requete->execute(array( $_GET["billet"]));
+while($donnees = $requete->fetch()){
 ?>
+<div class="commentaire">
+ <p><?php echo  $donnees["pseudo"];?></p>
+ <p><?php echo $donnees["commentaire"];?></p>
+ <p><?php echo $donnees["date_commentaire"];?></p>
+</div>
+<?php
+ }
+?>
+</body>
+</html>
+
+
+
+
+
+
